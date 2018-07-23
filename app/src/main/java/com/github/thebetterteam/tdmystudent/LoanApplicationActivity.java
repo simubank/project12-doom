e9b242239830b664036c5c8b193dd28d6a46039d
LeicaSimile <leicasimile@gmail.com>
2018-07-23 11:17:05 -0400

    Add loan application activity

---
+++ app/src/main/java/com/github/thebetterteam/tdmystudent/LoanApplicationActivity.java
@@ -0,0 +1,13 @@
+package com.github.thebetterteam.tdmystudent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoanApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_application);
+    }
+}
