5ed73f4ef09c2dc7f94e3f8ac9af38095f9b1258
LeicaSimile <leicasimile@gmail.com>
2018-07-23 17:22:28 -0400

    Add menu button event listeners in goal activity

--- app/src/main/java/com/github/thebetterteam/tdmystudent/GoalActivity.java
+++ app/src/main/java/com/github/thebetterteam/tdmystudent/GoalActivity.java
@@ -1,7 +1,10 @@
 package com.github.thebetterteam.tdmystudent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class GoalActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class GoalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+
+        LinearLayout btnHome = findViewById(R.id.goal_btnHome);
+        LinearLayout btnApplyLoan = findViewById(R.id.goal_btnApplyLoan);
+        LinearLayout btnRefer = findViewById(R.id.goal_btnRefer);
+
+        // Event-listeners
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayMainActivity();
+            }
+        });
+
+        btnApplyLoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayLoanApplicationActivity();
+            }
+        });
+
+        btnRefer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayReferralActivity();
+            }
+        });
+    }
+
+    private void displayMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    private void displayLoanApplicationActivity() {
+        Intent i = new Intent(this, LoanApplicationActivity.class);
+        startActivity(i);
+    }
+
+    private void displayReferralActivity() {
+        Intent i = new Intent(this, ReferralActivity.class);
+        startActivity(i);
     }
 }
