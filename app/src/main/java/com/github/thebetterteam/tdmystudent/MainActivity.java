09ba311a664466245f875a77aaef4df74739039c
LeicaSimile <leicasimile@gmail.com>
2018-07-23 17:12:03 -0400

    Add redirect functionality to menu buttons in MainActivity

--- app/src/main/java/com/github/thebetterteam/tdmystudent/MainActivity.java
+++ app/src/main/java/com/github/thebetterteam/tdmystudent/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.thebetterteam.tdmystudent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout btnGoalDetails = findViewById(R.id.main_btnGoalDetails);
+        LinearLayout btnApplyLoan = findViewById(R.id.main_btnApplyLoan);
+        LinearLayout btnRefer = findViewById(R.id.main_btnRefer);
+
+        // Event-listeners
+        btnGoalDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayGoalActivity();
+            }
+        });
+
+        btnApplyLoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayLoanApplicationActivity();
+            }
+        });
+
+        btnRefer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayReferralActivity();
+            }
+        });
+    }
+
+    private void displayGoalActivity() {
+        Intent i = new Intent(this, GoalActivity.class);
+        startActivity(i);
+    }
+
+    private void displayLoanApplicationActivity() {
+        Intent i = new Intent(this, LoanApplicationActivity.class);
+        startActivity(i);
+    }
+
+    private void displayReferralActivity() {
+        Intent i = new Intent(this, ReferralActivity.class);
+        startActivity(i);
     }
 }
