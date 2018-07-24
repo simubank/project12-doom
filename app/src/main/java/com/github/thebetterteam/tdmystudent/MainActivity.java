6cf8c0899c798c48a721368878e25a58da6729f4
wymbpe <yuminwu1997@gmail.com>
2018-07-23 22:30:29 -0400

    set up api and demo button

--- app/src/main/java/com/github/thebetterteam/tdmystudent/MainActivity.java
+++ app/src/main/java/com/github/thebetterteam/tdmystudent/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout btnGoalDetails = findViewById(R.id.main_btnGoalDetails);
         LinearLayout btnApplyLoan = findViewById(R.id.main_btnApplyLoan);
         LinearLayout btnRefer = findViewById(R.id.main_btnRefer);
+        Button btnCus = findViewById(R.id.main_btnCus);
 
         // Event-listeners
         btnGoalDetails.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
                 displayReferralActivity();
             }
         });
+
+        btnCus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayCustomerInfo();
+            }
+        });
+
     }
 
     private void displayGoalActivity() {
@@ -52,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayReferralActivity() {
         Intent i = new Intent(this, ReferralActivity.class);
+        startActivity(i);
+    }
+
+    private void displayCustomerInfo() {
+        Intent i = new Intent(this, CustomerInfo.class);
         startActivity(i);
     }
 }
